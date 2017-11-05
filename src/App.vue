0cf62cd2c50c0777f2ab78a8c9d1adc21d58cd0a
smook <243783062@qq.com>
2017-11-05 17:47:03 +0800

    first add

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item"><router-link to="/goods">商品</router-link></div>
+      <div class="tab-item"><router-link to="/ratings">评论</router-link></div>
+      <div class="tab-item"><router-link to="/seller">商家</router-link></div>
+    </div>
+    <keep-alive>
+      <router-view :seller="seller" keep-alived></router-view>
+    </keep-alive> 
+  </div>
+</template>
+
+<script>
+import header from 'components/header/header';
+import {urlParse} from 'common/js/util';
+const ERR_OK = 0;
+export default {
+  data() {
+    return {
+      seller: {
+        id: (() => {
+          let queryParam = urlParse();
+          return queryParam.id;
+        })()
+      }
+    };
+  },
+  created() {
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body;
+      if (response.errno === ERR_OK) {
+        this.seller = Object.assign({}, this.seller, response.data);
+        console.log(this.seller);
+      }
+    });
+  },
+  components: {
+    'v-header': header
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/mixin.styl"
+  .tab
+    display:flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    border-1px(rgba(7,17,27,0.1))
+    .tab-item
+      flex:1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77,85,93)
+        &.active
+            color: rgb(200, 20, 20)
+</style>
